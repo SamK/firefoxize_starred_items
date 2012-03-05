35d3e1294b4b0ac69fdf7058c48b1b26bf5d7c1b
Samuel Krieg <samyboy1+github@gmail.com>
2012-03-05 16:54:10 +0100

    Ooops

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     author=['Samuel Krieg'],
     author_email=['my firstname my lastname @ gmail.com'],
     name='firefoxize-starred-items',
-    description='converts your starred items in Google Reader into bookmarks for Firefox'
+    description='converts your starred items in Google Reader into bookmarks for Firefox',
     version='0.9',
     py_modules=['firefoxize-starred-items'],
     # requires: json', 'time', 'codecs' 'sys' 'os' 'argparse'],
