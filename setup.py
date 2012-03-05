cd941177532e66c125dfaf3c6998b89bf0bf8cc5
Samuel Krieg <samyboy1+github@gmail.com>
2012-03-05 16:52:30 +0100

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    author=['Samuel Krieg'],
+    author_email=['my firstname my lastname @ gmail.com'],
+    name='firefoxize-starred-items',
+    description='converts your starred items in Google Reader into bookmarks for Firefox'
+    version='0.9',
+    py_modules=['firefoxize-starred-items'],
+    # requires: json', 'time', 'codecs' 'sys' 'os' 'argparse'],
+)
